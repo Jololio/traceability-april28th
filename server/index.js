d1e6b3a8c6d9eba54bbc41b36262ada34ad2e38d
Joseph Al-Abudi <josephalabudi@gmail.com>
2022-04-28 15:35:42 -0600

    changed the rollbar stuff

--- server/index.js
+++ server/index.js
@@ -3,17 +3,24 @@ const path = require('path')
 
 const app = express()
 
-var Rollbar = require('rollbar')
+var Rollbar = require("rollbar");
 var rollbar = new Rollbar({
-    accessToken: '82ac0e4c040843d0917d939e9a7b1f4d',
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-})
+  accessToken: '50417134db5748ae9cfecb196c6ad2c2',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
 
 rollbar.log('Hello world!')
 
+rollbar.log()
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+
+app.get('/function', (req, res) =>{
+    niceFunction()
 })
 
 const port = process.env.PORT || 4545
